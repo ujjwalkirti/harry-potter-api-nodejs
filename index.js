3158fbeb64cdb9ce4e83c62e59191b47601f0595
ujjwalkirti <kirti.chargers@gmail.com>
2022-10-02 10:25:56 +0530

    02-10-2022

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.get("/", (req, res) => {
   res.render("home.ejs");
 });
 
+//get all the characters
+
+app.get("/all", (req, res) => {
+  const data = [];
+  res.status(200).json(data);
+});
+
 app.listen(PORT, () => {
   console.log(`server has started on port ${PORT}`);
 });
