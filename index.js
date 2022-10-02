6b29f2b1c68554bb290023684eba94a1e736c10f
ujjwalkirti <kirti.chargers@gmail.com>
2022-10-02 11:43:13 +0530

    02-10-2022

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const characterRouter = require("./routes/characters");
 const housesRouter = require("./routes/houses");
 const PORT = process.env.PORT || 6000;
 
+app.use(express.urlencoded({ extended: true }));
+
 //connect to MongoDB cluster
 db.connectToServer();
 
@@ -13,36 +15,69 @@ db.connectToServer();
 app.use("/characters", characterRouter);
 app.use("/houses", housesRouter);
 
+//render the homepage
 app.get("/", (req, res) => {
-  const routes = getTop50Records();
+  const routes = getTop50Records("routes");
   console.log(routes);
   res.render("home.ejs", { routes });
+});
+
+//render the page to add routes
+app.get("/addRoutes", (req, res) => {
+  res.render("addRoutes.ejs");
+});
+
+app.post("/addRoutes", (req, res) => {
+  const addedRoute = writeTheData("routes", req.body.routes);
+  res.send(req.body.routes);
 });
 
 app.listen(PORT, () => {
   console.log(`server has started on port ${PORT}`);
 });
 
-function getTop50Records() {
-  let gift = { msg: "", data: "", status: 0 };
-  db.route("/listings").get(async function (req, res) {
-    const dbConnect = dbo.getDb();
+/*----------------------UTILITY FUNCTIONS---------------------------*/
 
-    dbConnect
-      .collection("routes")
-      .find({})
-      .limit(50)
-      .toArray(function (err, result) {
-        if (err) {
-          gift.status = 404;
-          gift.msg = "Error fetching listings!";
-          gift.data = err;
-        } else {
-          gift.data = result;
-          gift.msg = "Successfully fetched the data";
-          gift.status = 200;
-        }
-      });
+function getTop50Records(collection) {
+  let gift = { msg: "", data: "", status: 0 };
+
+  const dbConnect = db.getDb();
+
+  dbConnect
+    .collection(collection)
+    .find({})
+    .limit(50)
+    .toArray(function (err, result) {
+      if (err) {
+        gift.status = 404;
+        gift.msg = "Error fetching listings!";
+        gift.data = err;
+      } else {
+        gift.data = result;
+        gift.msg = "Successfully fetched the data";
+        gift.status = 200;
+      }
+    });
+
+  return gift;
+}
+
+function writeTheData(collection, data) {
+  let gift = { msg: "", data: "", status: 0 };
+
+  const dbConnect = db.getDb();
+
+  dbConnect.collection(collection).insertOne(data, function (err, result) {
+    if (err) {
+      gift.status = 404;
+      gift.msg = "Error fetching listings!";
+      gift.data = err;
+    } else {
+      gift.data = result;
+      gift.msg = "Successfully fetched the data";
+      gift.status = 200;
+    }
   });
+
   return gift;
 }
