aaa0cf4cf095c540f7d6665379810430171efe85
ujjwalkirti <kirti.chargers@gmail.com>
2022-11-13 15:29:12 +0530

    changed the readme

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 function connectToDB() {
-  const connection = mongoose.connect("mongodb://localhost:27017/test");
+  const connection = mongoose.connect(process.env.MONGODB_URI);
   connection
     .then(console.log("connected to db successfully"))
     .catch((reason) => console.log(reason));
